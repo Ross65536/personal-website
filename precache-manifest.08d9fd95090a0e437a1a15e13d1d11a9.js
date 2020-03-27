f5aa9571c306eaae88c8d07c1d9328eba330c09f
ros <rk65536@protonmail.com>
2020-03-27 02:28:19 +0000

    Updates

---
+++ precache-manifest.08d9fd95090a0e437a1a15e13d1d11a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bfc003f49798dbecda8128808867356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbb304c87efa956618c3",
+    "url": "/static/css/main.ebb73fb8.chunk.css"
+  },
+  {
+    "revision": "8a4230602479bcadaa9f",
+    "url": "/static/js/2.f370dc1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f370dc1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb304c87efa956618c3",
+    "url": "/static/js/main.a2284979.chunk.js"
+  },
+  {
+    "revision": "0b4f3269348c2e5315e5",
+    "url": "/static/js/runtime-main.9089ee33.js"
+  }
+]);
