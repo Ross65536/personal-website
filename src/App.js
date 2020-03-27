1e1551a5a757afb8ae76889829f3d7d027805112
ros <rk65536@protonmail.com>
2020-03-27 02:14:49 +0000

    add basic page stubs, website layout

--- src/App.js
+++ src/App.js
@@ -1,41 +1,72 @@
-import React from 'react';
 import './App.scss';
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import About from './pages/about.js'
+import Projects from './pages/projects.js'
+import Resume from './pages/resume.js'
 
 const NAME = "Rostyslav Khoptiy"
 const PROFESSION = "Software Engineer"
+const GITHUB_URL = "https://github.com/Ross65536"
+const LINKEDIN_URL = "https://www.linkedin.com/in/rostyslav-khoptiy/";
+const ANDROID_PLAY_STORE = "https://play.google.com/store/apps/developer?id=Ros+Software";
+const EMAIL = "rk65536@protonmail.com"
 
 function App() {
   return (
-    <div className="App">
-      <header id="header">
-            <h1 id="name">
-                <a href="#"> {NAME} </a>
-            </h1>
-            <h3 id="profession">
-                 {PROFESSION}
-            </h3>
-            <nav id="top-bar">
-                <ul>
-                    <li><a href="{% url 'projects' %}">Projects</a></li>
-                    <li><a href="{% url 'resume' %}">Resume</a></li>
-                    <li><a href="{% url 'about' %}">About</a></li>
-                </ul>
-            </nav>
-        </header>
-        <div id="page-content">
-            BODY PAGE
-        </div>
-        <footer id="footer">
+    <div className="App" id="main">
+      <Router>
+
+        {/* HEADER */}
+        <header id="header">
+          <h1 id="name">
+            <Link to="/">{NAME}</Link>
+          </h1>
+          <h3 id="profession">
+            {PROFESSION}
+          </h3>
+          <nav id="top-bar">
             <ul>
-                {/* {% for link in person.footer_links.all %}
-                    <li><a href="{{ link.link }}">{% include 'personalPages/components/anchor-icon.html' with ref=link.link %}</i> {{ link.name }}</a></li>
-                {% endfor %}
-                {% if person.email %}
-                    <li><a href="mailto:{{ person.email.link }}">{% include 'personalPages/components/anchor-icon.html' with ref=person.email.link %} {{ person.email.name }}</a></li>
-                {% endif %} */}
+              <li><Link to="/">Projects</Link></li>
+              <li><Link to="/resume">Resume</Link></li>
+              <li><Link to="/about">About</Link></li>
             </ul>
+          </nav>
+        </header>
+
+        {/* BODY */}
+        <div id="page-content">
+          <Switch>
+            <Route exact path="/">
+              <Projects />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/resume">
+              <Resume />
+            </Route>
+          </Switch>
+        </div>
+
+        {/* FOOTER */}
+        <footer id="footer">
+          <ul>
+          <li><a href={GITHUB_URL}><i class="fab fa-github"/> GitHub </a></li>
+          <li><a href={LINKEDIN_URL}><i class="fab fa-linkedin"/> LinkedIn </a></li>
+          <li><a href={"mailto:" + EMAIL}><i class="fas fa-envelope"/> E-Mail</a></li>
+          <li><a href={ANDROID_PLAY_STORE}><i class="fab fa-google-play"/> Android Store </a></li>
+          </ul>
         </footer>
 
+      </Router>
     </div>
   );
 }
---
+++ src/pages/resume.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Resume() {
+  return (
+    <div>
+      <h2>Resume</h2>
+
+        AAA
+    </div>
+  );
+}
