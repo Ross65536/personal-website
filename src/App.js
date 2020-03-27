217299fe02cd802436527a8aa6f223e9f2c7b38a
ros <rk65536@protonmail.com>
2020-03-27 01:24:40 +0000

    add basic layout migration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+const NAME = "Rostyslav Khoptiy"
+const PROFESSION = "Software Engineer"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header id="header">
+            <h1 id="name">
+                <a href="#"> {NAME} </a>
+            </h1>
+            <h3 id="profession">
+                 {PROFESSION}
+            </h3>
+            <nav id="top-bar">
+                <ul>
+                    <li><a href="{% url 'projects' %}">Projects</a></li>
+                    <li><a href="{% url 'resume' %}">Resume</a></li>
+                    <li><a href="{% url 'about' %}">About</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id="page-content">
+            BODY PAGE
+        </div>
+        <footer id="footer">
+            <ul>
+                {/* {% for link in person.footer_links.all %}
+                    <li><a href="{{ link.link }}">{% include 'personalPages/components/anchor-icon.html' with ref=link.link %}</i> {{ link.name }}</a></li>
+                {% endfor %}
+                {% if person.email %}
+                    <li><a href="mailto:{{ person.email.link }}">{% include 'personalPages/components/anchor-icon.html' with ref=person.email.link %} {{ person.email.name }}</a></li>
+                {% endif %} */}
+            </ul>
+        </footer>
+
     </div>
   );
 }
