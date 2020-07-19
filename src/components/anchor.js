eba45f75b892e30b912ffe58bd37a794c063acc9
Rostyslav Khoptiy <rk65536@protonmail.com>
2020-07-19 15:40:15 +0100

    add layout code

---
+++ src/components/anchor.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const ICONS = {
+  none: <i class="fas fa-link"></i>,
+  github: <i class="fab fa-github"/>,
+  linkedin: <i class="fab fa-linkedin"/>,
+  email: <i class="fas fa-envelope"/>,
+  android_store: <i class="fab fa-google-play"/>,
+  wikipedia: <i class="fab fa-wikipedia-w"/>,
+};
+
+
+export default class Anchor extends React.Component {
+
+  render() {
+    return (
+      <a key={this.props.url} href={this.props.url}>{this.props.icon} {this.props.name} </a>
+    );
+
+  }
+}
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,41 +5,70 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { Link } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import './App.scss';
+import './header.scss';
+import './footer.scss';
+
+import CONSTANTS from './constants.js';
+import Anchor, { ICONS } from './anchor.js';
+import profile from './profile.jpeg'
+
+const NAME = CONSTANTS.name;
+const PROFESSION = "Software Engineer";
+const GITHUB_URL = "https://github.com/Ross65536";
+const LINKEDIN_URL = "https://www.linkedin.com/in/rostyslav-khoptiy/";
+const ANDROID_PLAY_STORE = "https://play.google.com/store/apps/developer?id=Ros+Software";
+const EMAIL = "rk65536@protonmail.com";
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <div className="App" id="main">
+      {/* HEADER */}
+      <div id="header-top">
+          <img src={profile} />
+          <header id="header">
+            <div>
+              <h1 id="name">
+                <Link to="./">{NAME}</Link>
+              </h1>
+              <h3 id="profession">
+                {PROFESSION}
+              </h3>
+            </div>
+
+            <nav id="top-bar">
+              <ul>
+                <li><Link to='./'>Projects</Link></li>
+                <li><Link to="resume">Resume</Link></li>
+                <li><Link to="about">About</Link></li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+
+        {/* BODY */}
+        <div id="page-content">
+          <main>{children}</main>
+        </div>
+
+        {/* FOOTER */}
+        <footer id="footer">
+          <ul>
+            <li><Anchor name="Github" url={GITHUB_URL} icon={ICONS.github} /></li>
+            <li><Anchor name="LinkedIn" url={LINKEDIN_URL} icon={ICONS.linkedin} /></li>
+            <li><Anchor name="E-Mail" url={"mailto:" + EMAIL} icon={ICONS.email} /></li>
+            {/* <li><Anchor name="Android Store" url={ANDROID_PLAY_STORE} icon={ICONS.android_store} /></li> */}
+          </ul>
         </footer>
-      </div>
+        </div>
     </>
   )
 }
