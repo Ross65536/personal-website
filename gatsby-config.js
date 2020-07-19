768e6082ce7bfebddb19d4f5b374a4978358f2c1
Rostyslav Khoptiy <rk65536@protonmail.com>
2020-07-19 16:28:08 +0100

    update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: `/personal-website`,
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My personal website`,
+    description: `My webspot.`,
+    author: `@ros`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
